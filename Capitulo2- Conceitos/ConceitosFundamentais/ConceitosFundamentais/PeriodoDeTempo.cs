58c2325ddcffce7058266784d96975abad4356f2
Gabriel Schade <gabriel.schade@hotmail.com>
2016-09-03 11:08:10 -0300

    Acoplamento temporal e dependencia de ordem

---
+++ Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais/PeriodoDeTempo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosFundamentais
+{
+    public class PeriodoDeTempo
+    {
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+
+        public bool VerificarSeDataEstaEntreOPeriodo(DateTime dataParaTestar)
+        {
+            return dataParaTestar.CompareTo(DataInicial) >= 0
+                    && dataParaTestar.CompareTo(DataFinal) <= 0;
+        }
+    }
+}
--- Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais/Program.cs
+++ Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais/Program.cs
@@ -10,6 +10,31 @@ namespace ConceitosFundamentais
     {
         static void Main(string[] args)
         {
+            PeriodoDeTempo periodo = new PeriodoDeTempo();
+
+            periodo.DataInicial = DateTime.Parse("20/08/2016");
+            periodo.DataFinal = DateTime.Parse("31/08/2016");
+
+            DateTime[] datasParaTeste = new DateTime[]
+            {
+                DateTime.Parse("18/08/2016"),
+                DateTime.Parse("22/08/2016"),
+                DateTime.Parse("01/09/2016")
+            };
+
+            foreach (DateTime dataParaTestar in datasParaTeste)
+            {
+                bool resultadoDaVerificacao =
+                periodo.VerificarSeDataEstaEntreOPeriodo(dataParaTestar);
+                Console.WriteLine(resultadoDaVerificacao);
+            }
+
+            Console.ReadKey();
         }
+
+
+
+
+
     }
 }
