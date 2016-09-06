46455e6fcade58134fd8b884fc6d582b21138120
Gabriel Schade <gabriel.schade@hotmail.com>
2016-09-06 00:29:13 -0300

    Alterações para o capítulo

---
+++ Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais/PeriodoDeTempoImutavel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosFundamentais
+{
+    public class PeriodoDeTempoImutavel
+    {
+        public DateTime DataInicial { get; private set; }
+        public DateTime DataFinal { get; private set; }
+
+        public PeriodoDeTempoImutavel(DateTime dataInicial, DateTime dataFinal)
+        {
+            DataInicial = dataInicial;
+            DataFinal = dataFinal;
+        }
+        
+        public bool VerificarSeDataEstaEntreOPeriodo(DateTime dataParaTestar)
+        {
+            return dataParaTestar.CompareTo(DataInicial) >= 0
+                    && dataParaTestar.CompareTo(DataFinal) <= 0;
+        }
+    }
+}
--- Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais/Program.cs
+++ Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais/Program.cs
@@ -10,6 +10,35 @@ namespace ConceitosFundamentais
     {
         static void Main(string[] args)
         {
+            ExemploDeProblemaPorAlteracaoDeEstado();
+        }
+
+        static void ExemploComPeriodoImutavel()
+        {
+            PeriodoDeTempoImutavel periodo = 
+                new PeriodoDeTempoImutavel( DateTime.Parse("20/08/2016")
+                                          , DateTime.Parse("31/08/2016") );
+
+            DateTime[] datasParaTeste = new DateTime[]
+            {
+                DateTime.Parse("18/08/2016"),
+                DateTime.Parse("22/08/2016"),
+                DateTime.Parse("01/09/2016")
+            };
+
+            Console.WriteLine("Resultado antes da alteração:");
+            foreach (DateTime dataParaTestar in datasParaTeste)
+            {
+                bool resultadoDaVerificacao =
+                periodo.VerificarSeDataEstaEntreOPeriodo(dataParaTestar);
+                Console.WriteLine(resultadoDaVerificacao);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ExemploDeAcoplamentoTemporalNaCriacaoDoObjeto()
+        {
             PeriodoDeTempo periodo = new PeriodoDeTempo();
 
             periodo.DataInicial = DateTime.Parse("20/08/2016");
@@ -32,8 +61,39 @@ namespace ConceitosFundamentais
             Console.ReadKey();
         }
 
+        static void ExemploDeProblemaPorAlteracaoDeEstado()
+        {
+            PeriodoDeTempo periodo = new PeriodoDeTempo();
 
+            periodo.DataInicial = DateTime.Parse("20/08/2016");
+            periodo.DataFinal = DateTime.Parse("31/08/2016");
 
+            DateTime[] datasParaTeste = new DateTime[]
+            {
+                DateTime.Parse("18/08/2016"),
+                DateTime.Parse("22/08/2016"),
+                DateTime.Parse("01/09/2016")
+            };
+
+            Console.WriteLine("Resultado antes da alteração:");
+            foreach (DateTime dataParaTestar in datasParaTeste)
+            {
+                bool resultadoDaVerificacao =
+                periodo.VerificarSeDataEstaEntreOPeriodo(dataParaTestar);
+                Console.WriteLine(resultadoDaVerificacao);
+            }
+
+            periodo.DataFinal = DateTime.MaxValue;
+            Console.WriteLine("Resultado após a alteração:");
+            foreach (DateTime dataParaTestar in datasParaTeste)
+            {
+                bool resultadoDaVerificacao =
+                periodo.VerificarSeDataEstaEntreOPeriodo(dataParaTestar);
+                Console.WriteLine(resultadoDaVerificacao);
+            }
+
+            Console.ReadKey();
+        }
 
 
     }
