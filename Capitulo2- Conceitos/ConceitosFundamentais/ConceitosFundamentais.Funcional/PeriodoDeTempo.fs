be18df92547600dfcd42ef79a9371e35c5e8d5fa
Gabriel Schade <gabriel.schade@hotmail.com>
2016-09-16 00:10:54 -0300

    Ajuste para capítulo 2

--- Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais.Funcional/PeriodoDeTempo.fs
+++ Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais.Funcional/PeriodoDeTempo.fs
@@ -5,13 +5,11 @@ type Periodo = {DataInicial:DateTime; DataFinal:DateTime}
 
 let adicionaDias periodo dias =
     { 
-      DataInicial = periodo.DataInicial.AddDays(dias) 
-      ; DataFinal=periodo.DataFinal.AddDays(dias)
+      DataInicial = periodo.DataInicial.AddDays dias 
+      ; DataFinal=periodo.DataFinal.AddDays dias
     }
 
 
-
-
 let verificarSeDataEstaEntreOPeriodo periodo (dataParaTestar:DateTime) = 
-    dataParaTestar.CompareTo(periodo.DataInicial) >= 0 && 
-    dataParaTestar.CompareTo(periodo.DataFinal) <= 0    
+    dataParaTestar.CompareTo periodo.DataInicial >= 0 && 
+    dataParaTestar.CompareTo periodo.DataFinal <= 0    
--- Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais.Funcional/Program.fs
+++ Capitulo2- Conceitos/ConceitosFundamentais/ConceitosFundamentais.Funcional/Program.fs
@@ -4,25 +4,20 @@ open System
 [<EntryPoint>]
 let main argv = 
     let periodo = 
-        { DataInicial= DateTime.Parse("20/08/2016"); 
-          DataFinal =DateTime.Parse("31/08/2016");}
+        { DataInicial= DateTime.Parse "20/08/2016"
+        ; DataFinal =DateTime.Parse "31/08/2016" }
 
     let datasParaTeste =
         [|
-            DateTime.Parse("18/08/2016")
-            DateTime.Parse("22/08/2016")
-            DateTime.Parse("01/09/2016")
+            DateTime.Parse "18/08/2016"
+            DateTime.Parse "22/08/2016"
+            DateTime.Parse "01/09/2016"
         |]
 
-    datasParaTeste 
-    |> Array.map (verificarSeDataEstaEntreOPeriodo periodo)
-    |> Array.iter (printfn "%b")
+    for data in datasParaTeste do
+        let dataEstaNoPeriodo = verificarSeDataEstaEntreOPeriodo periodo data
+        printfn "%b" dataEstaNoPeriodo
 
-//    periodo <- adicionaDias periodo 2.0
-
-    datasParaTeste 
-    |> Array.map (verificarSeDataEstaEntreOPeriodo periodo)
-    |> Array.iter (printfn "%b")
     Console.ReadKey() |> ignore
     0
 
