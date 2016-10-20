e0a85a6becc664c03047d57f375ad3aa34ef64de
Gabriel Schade <gabriel.schade@hotmail.com>
2016-10-20 00:41:42 -0200

    Commit inicial do cap3

--- Capitulo3- Funcoes/Funcoes.Funcional/Funcoes.Funcional/Program.fs
+++ Capitulo3- Funcoes/Funcoes.Funcional/Funcoes.Funcional/Program.fs
@@ -1,7 +1,12 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿
+let dez = 10
+
+let somaCom5 numero = numero + 5
+
+
+
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    0 // return an integer exit code
+    0
