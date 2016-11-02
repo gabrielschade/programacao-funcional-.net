7102af0994d9c66d023fb1792b4217e9f163e9d5
Gabriel Schade <gabriel.schade@hotmail.com>
2016-11-02 02:31:39 -0200

    Continuação

--- Capitulo3- Funcoes/Funcoes.Funcional/Funcoes.Funcional/FuncaoComoMembroDePrimeiraOrdem.fs
+++ Capitulo3- Funcoes/Funcoes.Funcional/Funcoes.Funcional/FuncaoComoMembroDePrimeiraOrdem.fs
@@ -5,41 +5,6 @@ let dez = 10
 
 let somaCom5 numero = numero + 5
 let somaCom5ViaLambda = fun numero -> numero + 5
-   
-// Versão 1 do Código
-//let imprimirNomes() =
-//    let nomes = ["Gabriel"; "Rodrigo"; "Jhony"; "Luan"]
-//    for nome in nomes do
-//        printfn "Olá %s." nome
-
-//let imprimirDobroDosNumeros numeros =
-//    let numeros = [1..10]
-//    for numero in numeros do
-//        printfn "%i." ( numero * 2)
-
-
-//Versão 2 do código
-//let imprimirNomes nomes =
-//    for nome in nomes do
-//        printfn "Olá %s." nome
-//
-//let imprimirDobroDosNumeros numeros =
-//    for numero in numeros do
-//        printfn "%i." ( numero * 2)
-
-
-let executarAcaoSobreElementos lista acao = lista |> List.iter acao
     
-let imprimirNomes nomes = 
-    executarAcaoSobreElementos nomes (printfn "Olá %s")    
-    
-let imprimirDobroDeUmNumero numero = 
-    printfn "%i." ( numero * 2)
+let referenciaParaSomaCom5 = somaCom5
 
-let imprimirDobroDosNumeros numeros = 
-    executarAcaoSobreElementos numeros (imprimirDobroDeUmNumero)    
-
-let imprimirDobroDosNumeros numeros = 
-    executarAcaoSobreElementos numeros 
-        (fun numero -> printfn "%i" (numero * 2) )    
-    
