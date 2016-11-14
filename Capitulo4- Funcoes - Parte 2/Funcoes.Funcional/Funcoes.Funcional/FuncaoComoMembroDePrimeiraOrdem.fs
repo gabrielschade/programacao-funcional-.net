9f9d6e669e495fbf0f796180dc1f7811498b2bd5
Gabriel Schade <gabriel.schade@hotmail.com>
2016-11-14 15:15:47 -0200

    Separação do código dos capítulos 3 e 4

---
+++ Capitulo4- Funcoes - Parte 2/Funcoes.Funcional/Funcoes.Funcional/FuncaoComoMembroDePrimeiraOrdem.fs
@@ -0,0 +1,10 @@
+﻿module FuncaoComoMembroDePrimeiraOrdem
+
+
+let dez = 10
+
+let somaCom5 numero = numero + 5
+let somaCom5ViaLambda = fun numero -> numero + 5
+    
+let referenciaParaSomaCom5 = somaCom5
+
---
+++ Capitulo4- Funcoes - Parte 2/Funcoes.Funcional/Funcoes/FuncaoComoMembroDePrimeiraOrdem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes
+{
+    class FuncaoComoMembroDePrimeiraOrdem
+    {
+        void AtribuindoFuncoes()
+        {
+            Func<int, int> referenciaParaSomaCom5 = SomaCom5;
+        }
+
+        int SomaCom5 (int numero)
+        {
+            return numero + 5;
+        }
+
+
+    }
+}
