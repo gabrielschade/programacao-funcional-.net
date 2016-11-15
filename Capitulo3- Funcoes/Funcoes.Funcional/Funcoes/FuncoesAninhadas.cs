2aa2d7a157d88f08032350cd7ab3a9a9da4c2cf3
Gabriel Schade <gabriel.schade@hotmail.com>
2016-11-14 23:39:37 -0200

    Atualização do código

---
+++ Capitulo3- Funcoes/Funcoes.Funcional/Funcoes/FuncoesAninhadas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcoes
+{
+    class FuncoesAninhadas
+    {
+        public void EscreveSeNumeroEParOuImpar(int numero)
+        {
+            Func<bool> verificaSeONumeroEPar = () => numero % 2 == 0;
+            Action escreveNumeroPar = 
+                () => Console.WriteLine(string.Format("O número {0} é par", numero));
+
+            Action escreveNumeroImpar =
+                () => Console.WriteLine(string.Format("O número {0} é impar", numero));
+
+            if (verificaSeONumeroEPar())
+                escreveNumeroPar();
+            else
+                escreveNumeroImpar();
+        }
+    }
+}
