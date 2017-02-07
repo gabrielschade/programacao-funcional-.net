e107ec5579fad99e7b337c4bc630c035db320574
Gabriel Schade <gabriel.schade@hotmail.com>
2017-02-07 01:16:50 -0200

    Avanço no capítulo de tipos

---
+++ Capitulo5- Tipos/Tipos/Tipos.Funcional/Records.fs
@@ -0,0 +1,17 @@
+﻿module Records
+
+type inteiroEBool = {valorInteiro:int ; valorBooleano:bool}
+
+let inteiroEBool = { valorInteiro = 2 ; valorBooleano = true}
+
+let {valorInteiro = inteiro ; valorBooleano = bool} = inteiroEBool
+
+let recordTransformadoEmTupla = 
+    inteiro,bool
+
+let {valorBooleano = booleano} = inteiroEBool
+
+let {valorInteiro = novoInteiro ; valorBooleano = novoBool} = inteiroEBool
+let segundoInteiroEBool = {valorInteiro = novoInteiro + 10 ; valorBooleano = not novoBool}
+
+let terceiroInteiroEBool = {inteiroEBool with valorBooleano = false}
