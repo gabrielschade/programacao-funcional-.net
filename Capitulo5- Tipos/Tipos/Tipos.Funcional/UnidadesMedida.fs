4cab5d6c550d17c1570ff963a3910247de23bc3e
Gabriel Schade <gabriel.schade@hotmail.com>
2017-02-28 07:24:03 -0300

    Continuação dos tipos

--- Capitulo5- Tipos/Tipos/Tipos.Funcional/UnidadesMedida.fs
+++ Capitulo5- Tipos/Tipos/Tipos.Funcional/UnidadesMedida.fs
@@ -19,3 +19,17 @@ let velocidadeDoSom = 340.29<m/s>
 
 [<Measure>] type velocidade = km/h
 [<Measure>] type velocidade2 = m/s
+
+let quilometros = 100.0<km>
+let metros = 1000.0<m>
+
+let fatorConversaoQuilometroPorMetro = 0.001<km/m>
+let metrosConvertidos = metros * fatorConversaoQuilometroPorMetro
+let soma = quilometros + metrosConvertidos
+
+let fatorConversaoMetroPorQuilometro = 1000.0<m/km>
+let quilometrosConvertidos = quilometros * fatorConversaoMetroPorQuilometro
+let somaEmMetros = quilometrosConvertidos + metros
+
+let inteiro = 30
+let quilometrosAPartirDeUmInteiro = inteiro * 1<km>
