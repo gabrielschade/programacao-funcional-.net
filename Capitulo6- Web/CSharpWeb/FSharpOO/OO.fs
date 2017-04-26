349b040f333b89b2cc0841003d6feb742163446f
Gabriel Schade <gabriel.schade@hotmail.com>
2017-04-26 17:36:52 -0300

    Finalização de OO em F# e inicio da aplicação web

--- Capitulo6- Web/CSharpWeb/FSharpOO/OO.fs
+++ Capitulo6- Web/CSharpWeb/FSharpOO/OO.fs
@@ -1,12 +1,17 @@
 ﻿module OO
 
+type IPessoa =
+    abstract member Nome:string
+    abstract member Sobrenome:string
+    abstract member NomeCompleto:string
+
 type Pessoa (nome:string, sobrenome:string) =
     member this.Nome = nome
     member this.Sobrenome = sobrenome 
     
 let gabriel = Pessoa("Gabriel", "Schade")
 
-gabriel.Nome <- "Teste"
+//gabriel.Nome <- "Teste"
 
 type PessoaComGetESet (nome:string, sobrenome:string) =
     let mutable _nome = nome
@@ -26,3 +31,37 @@ gabriel2.Sobrenome <- "Schade2"
 type PessoaComAutoproperty (nome:string, sobrenome:string) =
     member val Nome = nome with get, set
     member val Sobrenome = sobrenome with get, set
+
+
+type PessoaComIdade (idade:int) =
+    let mutable _maioridade = false
+    do
+        _maioridade <- idade >= 18
+
+    member val Idade = idade with get, set
+    member val Maioridade = _maioridade with get
+
+type PessoaComHeranca(nome:string, sobrenome:string, idade:int) =
+    inherit PessoaComAutoproperty(nome, sobrenome)
+    
+    let mutable _maioridade = false
+
+    do
+        _maioridade <- idade >= 18
+
+    member val Idade = idade with get, set
+    member val Maioridade = _maioridade with get
+
+let gabriel3 = PessoaComHeranca("Gabriel", "Schade", 26)
+
+type PessoaComInterface (nome:string, sobrenome:string) =
+    member val Nome = nome with get, set
+    member val Sobrenome = sobrenome with get, set
+
+    interface IPessoa with
+        member this.Nome = nome
+        member this.Sobrenome = sobrenome
+        member this.NomeCompleto = sprintf "%s %s" nome sobrenome
+
+let gabriel4 = PessoaComInterface("Gabriel", "Schade")
+let nomeCompleto = (gabriel4 :> IPessoa).NomeCompleto
