cf75527d08167a4af6721fe079bf01c31f07821b
Gabriel Schade <gabriel.schade@hotmail.com>
2017-04-27 11:53:08 -0300

    Template vazio e alterações nos projetos web

---
+++ Capitulo6- Web/FSharpWeb/ConsoleApplication1/Json.fs
@@ -0,0 +1,12 @@
+﻿module Json
+
+open Newtonsoft.Json
+
+/// Object to Json 
+let internal json<'t> (myObj:'t) =   
+         JsonConvert.SerializeObject(myObj)
+
+
+/// Object from Json 
+let internal unjson<'t> (jsonString:string)  : 't =  
+        JsonConvert.DeserializeObject<'t>(jsonString)
---
+++ Capitulo6- Web/FSharpWeb/ConsoleApplication1/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open FSharp.Data
+open System.IO
+open Json 
+
+type X = { Id : int ; AxisNumber:int}
+[<EntryPoint>]
+let main argv = 
+    let json = File.ReadAllText(@"D:\Documentos\GitHub\programacao-funcional-.net\Capitulo6- Web\FSharpWeb\ConsoleApplication1\data.json")
+    let valor = Json.unjson json
+    printf "%i %i" valor.AxisNumber valor.Id
+    let valor2 = { valor with Id = 10}
+    let texto = Json.json valor2
+    File.WriteAllText(@"D:\Documentos\GitHub\programacao-funcional-.net\Capitulo6- Web\FSharpWeb\ConsoleApplication1\data2.json",texto)
+    printfn "%A" argv
+    0 // return an integer exit code
