24a309d2933f48c98e1f2b520474da9e25b12dcf
Gabriel Schade <gabriel.schade@hotmail.com>
2017-04-28 19:37:59 -0300

    Unificação da solution no projeto

--- Capitulo7- Dados e colecoes/FSharpPersistencia/BancoDados.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace FSharpWeb.Persistencia
-
-module BancoDados =
-    let escrever nome = printfn "seu nome é: %s" nome
---
+++ Capitulo7- Dados e colecoes/FSharpWeb/Persistencia.fs
@@ -0,0 +1,26 @@
+﻿module Persistencia
+
+open Operadores
+
+type Tabela<'e> = {
+    Arquivo: string
+    Dados: 'e list
+}
+
+type Contexto = {
+    Inteiros: Tabela<int>
+    Textos: Tabela<string>
+}
+
+let inicializarTabela diretorioBase arquivo dadosIniciais =
+    {
+        Arquivo = diretorioBase ^ arquivo
+        Dados = dadosIniciais
+    }
+
+//let x = inicializarTabela<int> "a" "b" List.empty
+//    let y = "a"  "b"
+    
+//let salvar<'e> (entidade: 'e) =
+//    FSharpWeb.Util.Wrappers.Json.serializar entidade
+//    |> Arquivo.salvar "C:"
