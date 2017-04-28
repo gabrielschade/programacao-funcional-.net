898a738360035cdfb2f121ea73833bea422b22ca
Gabriel Schade <gabriel.schade@hotmail.com>
2017-04-28 12:13:42 -0300

    Atualizações da solução com os wrappers

--- Capitulo7- Dados e colecoes/ConsoleApplication1/AssemblyInfo.fs
+++ Capitulo7- Dados e colecoes/ConsoleApplication1/AssemblyInfo.fs
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("e5b3aa00-777d-4cbe-ba5d-3ee43595927b")>]
+[<assembly: Guid("28566f88-1111-404b-921d-ef23c9a60577")>]
 
 // Version information for an assembly consists of the following four values:
 // 
---
+++ Capitulo7- Dados e colecoes/ConsoleApplication1/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open FSharpWeb.Persistencia
+open FSharpWeb.Persistencia.BancoDados
+
+[<EntryPoint>]
+let main argv = 
+    let teste = {
+        LocalArquivo = @"D:\Documentos\F#\oi.json"; 
+        Entidade = 1}
+
+    Wrappers.Json.serializar teste.Entidade
+    |> Wrappers.Arquivo.salvar teste.LocalArquivo
+    
+    let entidade=
+        Wrappers.Arquivo.abrir teste.LocalArquivo
+        |> Wrappers.Json.desserializar
+
+    printfn "%A" argv
+    0 // return an integer exit code
