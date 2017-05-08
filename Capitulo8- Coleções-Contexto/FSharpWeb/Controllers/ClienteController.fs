241327692647e142f4e435d7ad46c3393dd0ff63
Gabriel Schade <gabriel.schade@hotmail.com>
2017-05-07 22:58:19 -0300

    Finalização do controller de clientes

---
+++ Capitulo8- Coleções-Contexto/FSharpWeb/Controllers/ClienteController.fs
@@ -0,0 +1,33 @@
+﻿namespace FSharpWeb.Controllers
+
+open System
+open System.Net.Http
+open System.Web.Http
+open Servicos
+
+type ClienteController() =
+    inherit ApiController()
+
+    [<HttpGet>]
+    member this.ObterTodos() = 
+       ClienteServico.obterTodos()
+
+    [<HttpGet>]
+    member this.ObterPorId(id) = 
+       ClienteServico.obterPorId id
+
+    [<HttpGet>]
+    member this.ObterPorNome(nome) = 
+       ClienteServico.obterPorNome nome
+
+    [<HttpPost>]
+    member this.Salvar(cliente) =
+        ClienteServico.incluirCliente cliente
+
+    [<HttpPut>]
+    member this.Atualizar(cliente) =
+        ClienteServico.atualizarCliente cliente
+
+    [<HttpDelete>]
+    member this.Excluir(id) =
+        ClienteServico.excluirCliente id
---
+++ Capitulo8- Coleções-Contexto/FSharpWeb/Servicos.fs
@@ -0,0 +1,52 @@
+﻿namespace Servicos
+
+open Dominio
+open Persistencia
+
+
+
+module ClienteServico =
+    let obterClientes() =
+        obterContexto().Clientes
+    
+    let atualizarTabelaClientes funcaoParaObterNovosDados =
+        let tabela = obterClientes()
+        let dados = funcaoParaObterNovosDados tabela
+        salvarTabela {tabela with Dados = dados}
+
+    let filtrarClientesPor filtro lista =
+        lista |> List.filter filtro
+
+    let filtrarTabelaClientesPor filtro =
+        obterClientes().Dados
+        |> filtrarClientesPor filtro
+
+    let excluirClienteComId id (lista:Cliente list) =
+           lista
+           |> filtrarClientesPor (fun cliente -> cliente.Id <> id)
+
+    let excluirCliente id =
+        atualizarTabelaClientes (fun tabela -> 
+            excluirClienteComId id tabela.Dados)
+
+    let incluirCliente cliente =
+        atualizarTabelaClientes (fun tabela -> cliente :: tabela.Dados)
+
+    let atualizarCliente cliente =
+        let removeEAdiciona tabela = 
+           cliente :: (excluirClienteComId 
+                        cliente.Id 
+                        tabela.Dados)
+
+        atualizarTabelaClientes (removeEAdiciona)
+
+    let obterTodos() =
+        obterClientes().Dados
+
+    let obterPorId id =
+        filtrarTabelaClientesPor 
+            (fun cliente -> cliente.Id = id)
+
+    let obterPorNome nome =
+        filtrarTabelaClientesPor
+            (fun cliente -> cliente.Nome.Contains nome)
